fd4cb209efcb21938d3bf2491bdb0566416c13a7
abdul <abdul@LAPTOP-34D7O5A0>
2023-03-15 11:32:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+//for loop-
+
+//int num, i, sum = 0;
+
+//for (num = 1; num <= 10; num++)
+
+//{
+//    for (i = 1; i <= num; i++) 
+//    {
+//        sum = sum + 1;
+
+//        Console.Write($"{i + "+"}");
+//    }
+//    Console.WriteLine($"=(Addition of nummbers: {sum}");
+//}
+
+//While loop
+
+//int i = 1;
+
+//while (i <= 10)
+
+//{
+//    Console.WriteLine("While loop: iterartion {0}", i);
+
+//    i++;
+//}
+
+//Do While loop-
+
+
+int j = 0;
+
+do
+{
+    Console.WriteLine("While loop: iterartion {0}", j);
+
+    j++;    
+
+} while (j < 10);
